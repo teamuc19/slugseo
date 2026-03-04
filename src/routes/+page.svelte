b73ed90a34ea60328c1a6f20e8d96d5b7424c792
Tea <TeaMuc19@htl-shkoder.com>
2026-03-04 12:48:15 +0100

    SEO PRoject

---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+    import background from '$lib/assets/background.jpg';
+</script>
+
+<svelte:head>
+    <title>Tea's Webshop - Electronics for all!</title>
+    <meta name= "description" content="We sell used electronics for a super cheap price!">
+</svelte:head>
+ 
+<header class="h-[500px] bg-center bg-cover flex items-center justify-center"
+    style="background-image: url({background})">
+    <h1 class="text-5xl text-white p-4">Electronic Devices</h1>
+</header>
+ 
+<main>
+ 
+</main>
+ 
+<footer>
+ 
+</footer>
